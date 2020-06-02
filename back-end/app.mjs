6eac1db30d82ad19769a19248768c93f273d02b2
markw13a <markw13a@gmail.com>
2020-06-02 23:58:04 +0100

    Changed port and cors

--- back-end/app.mjs
+++ back-end/app.mjs
@@ -1,6 +1,8 @@
 import Koa from "koa";
+import cors from "@koa/cors";
 import { router } from "./routes/router";
 
 const app = new Koa();
+app.use(cors());
 app.use(router.routes());
-app.listen(3000);
+app.listen(4000);
