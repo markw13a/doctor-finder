abf59c34d8a3a81c60d4340ec3248dec7a68f6a4
markw13a <markw13a@gmail.com>
2020-06-05 19:04:01 +0100

    Preparing to serve via Heroku

--- back-end/app.mjs
+++ back-end/app.mjs
@@ -1,8 +1,10 @@
 import Koa from "koa";
 import cors from "@koa/cors";
+import serve from "koa-static";
 import { router } from "./routes/router";
 
 const app = new Koa();
 app.use(cors());
 app.use(router.routes());
+app.use(serve("../front-end/build"));
 app.listen(4000);
---
+++ back-end/credentials.mjs
@@ -0,0 +1,9 @@
+// This is not something that you would want to expose in a production app
+// Have removed credentials.js from .gitignore in order to simplify deployment
+const user = "phloTest0@gmail.com";
+const pass = "phloTest123!";
+
+export {
+    user,
+    pass
+};
