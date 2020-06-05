bf40f310fb35fb79de8eb693333a869fd3169089
markw13a <markw13a@gmail.com>
2020-06-05 19:20:35 +0100

    This one appears to work locally

--- app.mjs
+++ app.mjs
@@ -6,5 +6,5 @@ import { router } from "./routes/router";
 const app = new Koa();
 app.use(cors());
 app.use(router.routes());
-app.use(serve("../front-end/build"));
+app.use(serve("./front-end/build"));
 app.listen(4000);
